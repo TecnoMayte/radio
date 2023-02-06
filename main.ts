7ca0cb9ba8144f73c9a0dfd0f9b56879f6825ec7
TecnoMayte <114059096+TecnoMayte@users.noreply.github.com>
2023-02-06 21:12:17 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,13 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    basic.showString("" + (receivedNumber))
-    basic.pause(1000)
-    basic.clearScreen()
-})
 input.onButtonPressed(Button.A, function () {
     radio.sendString("A")
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("A+B")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+    basic.pause(100)
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendString("B")
